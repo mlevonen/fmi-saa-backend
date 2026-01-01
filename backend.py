ab6c9a1cf5f1874d85a5521e7d7e640cbeea4b6c
mlevonen <65166184+mlevonen@users.noreply.github.com>
2026-01-01 22:30:13 +0200

    Add files via upload

--- backend.py
+++ backend.py
@@ -1,4 +1,10 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+from flask import Flask, jsonify
 import requests
 import xml.etree.ElementTree as ET
 
